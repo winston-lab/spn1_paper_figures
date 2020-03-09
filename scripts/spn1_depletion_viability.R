f6cf55690c2a74c48659f84f5f918e91f4dbfb90
James Chuang <chuang.ja@gmail.com>
2020-03-09 12:33:15 -0400

    Update spn1 depletion viability.

--- scripts/spn1_depletion_viability.R
+++ scripts/spn1_depletion_viability.R
@@ -1,6 +1,6 @@
 
 main = function(theme_path = "spn1_2020_theme.R",
-                data_path = "viability_total_col_per_mL_rel_T0.txt",
+                data_path = "viability_data.txt",
                 pdf_out="test.pdf",
                 grob_out="test.Rdata",
                 fig_width=6,
@@ -8,8 +8,9 @@ main = function(theme_path = "spn1_2020_theme.R",
                 panel_letter="x"){
     source(theme_path)
 
-    df = read_tsv(data_path) %>%
-        rename(cfu_per_ml_rel_t0=`colonies per mL rel. to T0`) %>%
+    df = read_tsv(data_path,
+                  col_types="ccidiiidd",
+                  na=c("N/A")) %>%
         mutate(strain = ordered(strain,
                                 levels=c("WT",
                                          "noAID",
@@ -18,15 +19,20 @@ main = function(theme_path = "spn1_2020_theme.R",
                                          "Spn1-untagged",
                                          "Spn1-AID")),
                condition = ordered(condition,
-                                   levels=c("DMSO",
+                                   levels=c("T0",
+                                            "DMSO",
                                             "IAA"),
-                                   labels=c("DMSO",
-                                            "IAA")))
+                                   labels=c("T0",
+                                            "DMSO",
+                                            "IAA")),
+               cfu_per_od_unit = `CFU/mL_normal` / OD600) %>%
+        filter(condition != "T0")
+
 
     df_summary = df %>%
         group_by(strain, condition) %>%
-        summarize(mean=mean(cfu_per_ml_rel_t0),
-                  sd=sd(cfu_per_ml_rel_t0))
+        summarize(mean = mean(cfu_per_od_unit, na.rm=TRUE),
+                  sd = sd(cfu_per_od_unit, na.rm=TRUE))
 
     viability_barplot = ggplot() +
         geom_col(data=df_summary,
@@ -51,10 +57,10 @@ main = function(theme_path = "spn1_2020_theme.R",
         geom_point(data=df,
                    aes(x=strain,
                        color=condition,
-                      y=cfu_per_ml_rel_t0),
+                      y=cfu_per_od_unit),
                    position=position_jitterdodge(dodge.width=0.8),
                    shape=16,
-                   size=0.7,
+                   size=0.5,
                    alpha=0.8) +
         scale_color_brewer(palette="Set1",
                           direction = -1,
@@ -65,9 +71,15 @@ main = function(theme_path = "spn1_2020_theme.R",
                            # guide=FALSE) +
         scale_y_continuous(limits=c(0,
                                     max(c(df_summary[["mean"]] + df_summary[["sd"]],
-                                          df[["cfu_per_ml_rel_t0"]])) * 1.05),
+                                          df[["cfu_per_od_unit"]]),
+                                        na.rm=TRUE) * 1.05),
                            expand=c(0,0),
-                           name="?????????") +
+                           name=expression(textstyle(frac("CFU", "OD"[600] ~ "units"))),
+                           breaks=c(0, 1e7, 2e7, 3e7),
+                           labels=c(expression(scriptstyle(0)),
+                                    expression(scriptstyle(1 %*% 10^7)),
+                                    expression(scriptstyle(2 %*% 10^7)),
+                                    expression(scriptstyle(3 %*% 10^7)))) +
         scale_x_discrete(name=NULL) +
         labs(tag=panel_letter) +
         theme_default +
@@ -77,7 +89,10 @@ main = function(theme_path = "spn1_2020_theme.R",
               legend.box.background=element_blank(),
               legend.margin=margin(l=-6, unit="pt"),
               axis.text.x=element_text(angle=20,
-                                       hjust=0.8))
+                                       hjust=0.8),
+              axis.title.y=element_text(angle=0,
+                                        vjust=0.5,
+                                        margin=margin(r=4, unit="pt")))
 
     ggsave(pdf_out,
            plot=viability_barplot,
@@ -96,3 +111,4 @@ main(theme_path=snakemake@input[["theme"]],
      fig_width=snakemake@params[["fig_width"]],
      fig_height=snakemake@params[["fig_height"]],
      panel_letter=snakemake@params[["panel_letter"]])
+
