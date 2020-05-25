a699fd7166526f75fecf0ce41a0d5fe0ac04e2a5
James Chuang <chuang.ja@gmail.com>
2020-05-25 08:59:06 -0400

    Make requested change of Ser5P and Ser2P to Ser5-P and Ser2-P.

--- scripts/rpb1_metagenes.R
+++ scripts/rpb1_metagenes.R
@@ -124,8 +124,8 @@ main = function(theme_path = "spn1_2020_theme.R",
                                       "ChIPseq-Ser2P-Rpb1norm"),
                              # labels=c("textstyle(frac(\"Rpb1-Ser5P\",\"Rpb1\"))",
                                       # "textstyle(frac(\"Rpb1-Ser2P\",\"Rpb1\"))")))
-                             labels=c("frac(\"Rpb1-Ser5P\",\"Rpb1\")",
-                                      "frac(\"Rpb1-Ser2P\",\"Rpb1\")")))
+                             labels=c("frac(\"Ser5-P\",\"Rpb1\")",
+                                      "frac(\"Ser2-P\",\"Rpb1\")")))
 
     modification_metagenes = ggplot(data=df_mods,
            aes(x=position,
